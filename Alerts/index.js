54d0295d856c6510bb690a48d2519f88652f7cb5
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-31 17:20:47 +0500

    👉Js execise

---
+++ Alerts/index.js
@@ -0,0 +1,36 @@
+// ===== Chapter 01 =====
+// ***Assignment #1 ***
+
+//Q 1
+alert("Error ! Please enter  a valid password.");
+
+// Q 2
+alert(" 👏WelCome to Js Land...\n Happy Coding! ❤");
+
+// Q 3
+alert("WelCome to Js Land...");
+
+// Q 4
+console.log("Hello , I can run Js through my web browser`s console");
+
+// Q 5
+
+// User Greetings
+var userName = prompt("Kindly enter your name :");
+alert("Thanks Mr : " + userName);
+
+// Q 6
+// Age Checker
+
+var ageCheck = +prompt(" Now Enter your age :");
+alert("Your age is : " + ageCheck + " years old");
+
+// Q 7
+//Short Quiz
+
+var q1 = prompt("HTML is stand for:");
+if (q1.toLowerCase() === "hyper text markup language") {
+  alert("Correct");
+} else {
+  alert("Incorrect Try Again");
+}
---
+++ Variables for Strings/index.js
@@ -0,0 +1,49 @@
+// ===== Chapter 02 =====
+// ***Assignment #2 ***
+// Q 1
+var userName;
+console.log(userName);
+// Q 2
+var myName = "Sohail Shoukat";
+console.log(myName);
+// Q 3
+var message = "Hello World !";
+alert(message);
+// Q 4
+var stdName = "Sohail";
+alert(stdName);
+var stdAge = 15;
+alert(stdAge + " years old");
+var stdRole = "Web and App development";
+alert(stdRole);
+
+// Q 5
+var pizza = "Pizza";
+alert(pizza);
+alert(pizza.slice(0, 4));
+alert(pizza.slice(0, 3));
+alert(pizza.slice(0, 2));
+alert(pizza.slice(0, 1));
+// Q 6
+var email = "example@example.com";
+alert("My Email Address is: " + email);
+// Q 6
+var book = "A smarter way to learn JavaScript";
+alert("Iam trying to learn from the book " + book);
+// Q 8
+var para = "Yah ! I can write HTML content through JS  </br>";
+document.write(para);
+// Q 9
+var pattern = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+document.write(pattern);
+
+// Q 10
+var first_name = "Sohail";
+var last_name = "Shoukat </br>";
+var full_name = first_name + " " + last_name;
+document.write(full_name);
+// Q 11
+var firstName = "Sohail Shoukat";
+var age = 21;
+var greetings = `My name is ${firstName} and I am ${age} Years old`;
+document.write(greetings);
