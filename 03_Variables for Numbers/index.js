85ae5c0ccd1f585f5dd984e60e67dd56d8357c8d
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-31 20:40:04 +0500

    👉Js exercise

---
+++ 03_Variables for Numbers/index.js
@@ -0,0 +1,42 @@
+// ===== Chapter 03 =====
+// ***Assignment #3 ***
+
+console.log("Hello World");
+
+// Q 1
+var age = 21;
+alert(`I am ${age} years old `);
+
+// Q 2
+var birthYear = "2006";
+alert(`My Birth year is ${birthYear}`);
+
+// Q 3
+var visitorName = prompt("Enter Your Name :");
+var quantity = +prompt("Number of Quantity");
+var productTitle = prompt("Enter Your Product");
+document.write(
+  visitorName +
+    " ordered" +
+    " " +
+    quantity +
+    " " +
+    productTitle +
+    " " +
+    "on Sohail online Store"
+);
+
+// Q 4
+//Task 1
+// sum, difference, product, and quotient
+
+var num1 = 10;
+var num2 = 10;
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
