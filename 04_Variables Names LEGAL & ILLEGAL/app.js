037899489492750353fcaec36c3e58257126d735
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-11-01 18:23:00 +0500

    👉Js exercise

---
+++ 04_Variables Names LEGAL & ILLEGAL/app.js
@@ -0,0 +1,17 @@
+// ===== Chapter 04 =====
+// ***Assignment #4 ***
+
+// Q 1
+var a = "1st",
+  b = "2nd",
+  c = "3rd";
+console.log(a, b, c);
+
+// Q 2
+
+var myName = "myName";
+console.log("Legal Variable :", myName);
+var my_name = "my_name";
+console.log("Legal Variabe :", my_name);
+var $myname = "$myname";
+console.log("Legal Variabe :", $myname);
---
+++ 05_Math Expressions/index.js
@@ -0,0 +1,67 @@
+// ===== Chapter 05 =====
+// ***Assignment #5 ***
+
+// Q 1
+var num1 = 3;
+var num2 = 5;
+var sum = num1 + num2;
+document.write(`Sum of 3 and and 5 is ${sum} <br/>`);
+
+// Q 2
+var num1 = 3;
+var num2 = 5;
+var subtraction = num1 - num2;
+document.write(`Subtractiom of 3 and and 5 is ${subtraction} <br/>`);
+
+// Q 3
+var num1 = 3;
+var num2 = 5;
+var multiply = num1 * num2;
+document.write(`Multiply of 3 and and 5 is ${multiply} <br/>`);
+
+// Q 4
+
+var num1 = 3;
+var num2 = 5;
+var division = num1 / num2;
+document.write(`division of 3 and and 5 is ${division} <br/>`);
+
+// Q 5
+var number = 5;
+document.write(`Initial Value is : ${number} <br/>`);
+number++;
+document.write(`Value after increment is: ${number} <br/>`);
+number = 5 + 8;
+document.write(`Value after addition is : ${number} <br/>`);
+--number;
+document.write(`Value after decrement is: ${number} <br/>`);
+number = number % 3;
+document.write(`The remainder is : ${number} <br/>`);
+
+// Q 6
+var movieTicket = "600";
+var totalTicket = 5;
+var product = movieTicket * totalTicket;
+document.write(
+  `Total Cost to buy 5 tickets to a movie is ${product} PKR <br/>`
+);
+
+// Q 7
+// Table of 4
+var tableNumber = 4;
+document.write(`4 X 1 = ${tableNumber * 1} </br>`);
+document.write(`4 X 2 = ${tableNumber * 2} </br>`);
+document.write(`4 X 3 = ${tableNumber * 3} </br>`);
+document.write(`4 X 4 = ${tableNumber * 4} </br>`);
+document.write(`4 X 5 = ${tableNumber * 5} </br>`);
+document.write(`4 X 6 = ${tableNumber * 6} </br>`);
+document.write(`4 X 6 = ${tableNumber * 7} </br>`);
+document.write(`4 X 7 = ${tableNumber * 8} </br>`);
+document.write(`4 X 8 = ${tableNumber * 9} </br>`);
+document.write(`4 X 9 = ${tableNumber * 10} </br>`);
+
+// Q 8
+
+//The Temperature Converter
+
+var Celsiustemperature = 37;
